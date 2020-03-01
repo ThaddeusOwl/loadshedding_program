1496162105af0c4128952f51fbd4dfa29f901d8f
Thaddeus Owl <61245401+ThaddeusOwl@users.noreply.github.com>
2020-03-01 12:53:53 +0200

    Added Data Class

---
+++ src/Data.java
@@ -0,0 +1,19 @@
+public class Data{
+
+private String  details;
+private String areas;
+
+public Data(String a, String b){
+	details=a;
+	areas=b;
+}
+
+public String getDetails(){
+	return details;
+}
+
+public String getAreas(){
+	return areas;
+}
+
+}
