7e06f465fef5db035c9a21155839cb030fc126be
Thaddeus Owl <61245401+ThaddeusOwl@users.noreply.github.com>
2020-03-01 15:50:22 +0200

    updated LSArray

--- src/LSArray.java
+++ src/LSArray.java
@@ -1,3 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LSArray{
+
+	Data[] dataArray;
+
+	public LSArray(){
+		this.dataArray = new Data[2976];
+
+		Scanner file=new Scanner(new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt"));
+		for (int i=0, i<dataArray.length(), i++){
+			if (file.hasNextLine()){
+				String line = file.nextLine();
+				String[] lineSplit = lineSplit(" ", 2);
+				dataArray[i] = new Data(lineSplit[0], lineSplit[1]);
+
+			}
+		}
+	}
+
+
 
 }
