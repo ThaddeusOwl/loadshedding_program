c9be50969464fcf4a4887d376a5eb2031a35bd44
Thaddeus Owl <61245401+ThaddeusOwl@users.noreply.github.com>
2020-03-03 12:41:27 +0200

    Randomized text file but it's very big

---
+++ src/RandomizeLSD.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RandomizeLSD{
+
+	public static void main(String [] args) throws FileNotFoundException{
+		List<String> lines= new ArrayList<String>(2976);
+		Scanner file = new Scanner(new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt"));
+		while(file.hasNextLine()){
+			String line = file.nextLine();
+			lines.add(line);
+
+		Collections.shuffle(lines);
+		for(String s:lines){
+         System.out.println(s);}
+		}
+	}
+
+
+}
